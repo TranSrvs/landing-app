c396d31e786b082d6c4e4011156b22326238335c
amoallim15 <amoallim15@gmail.com>
2023-07-03 19:50:17 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/landing-app"
+  basePath: "/landing-app",
+  assetPrefix: "/landing-app"
 };
 
 module.exports = nextConfig;
